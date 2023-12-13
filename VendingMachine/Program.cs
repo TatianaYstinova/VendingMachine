6a07096342c7592840c6dd93fcffc872cdb450d3
ustinova <rykova.tanya.96@gmail.com>
2023-12-13 17:21:20 +0300

    Add change

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -1,5 +1,11 @@
 ﻿using SaleOfCoffee;
 VendingMachine vendingMachine = new VendingMachine();
+
+
+
+
+
+
 Console.WriteLine("Выберите номер товара");
 Console.WriteLine("1 - Латте");
 Console.WriteLine("2 - Капучино");
@@ -13,18 +19,16 @@ do
     {
         case 1:
             Drink latte = new Drink("Латте", 100, 50, 10, 123);
-            vendingMachine.DisplayPrice(latte);
             vendingMachine.BuyDrink(latte);
             
             break;
         case 2:
             Drink cappuccino = new Drink("Капучино", 120, 40, 10, 150);
-            vendingMachine.DisplayPrice(cappuccino);
+            
             vendingMachine.BuyDrink(cappuccino);
             break;
         case 3:
             Drink tea = new Drink("Чай черный", 100, 0, 0, 50);
-            vendingMachine.DisplayPrice(tea);
             vendingMachine.BuyDrink(tea);
             break;
         case 0:
@@ -36,10 +40,5 @@ do
 } while (choice != 0);
 
 
-NumberOfCoins.Add(1, 10);
-NumberOfCoins.Add(2, 50);
-NumberOfCoins.Add(5, 20);
-NumberOfCoins.Add(10, 50);
-NumberOfCoins.Add(50, 20);
-NumberOfCoins.Add(100, 50);
+
 
--- VendingMachine/VendingMachine,.cs
+++ VendingMachine/VendingMachine,.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -18,49 +19,83 @@ namespace SaleOfCoffee
         public int Earnings { get;set; }
         Dictionary<int,int> NumberOfCoins { get; set; }
 
+        public  void InfoVendingMachine()
+        {
+            CashBalance = 0;
+            WaterResidue = 0;
+            LeftoverCoffee = 0;
+            MilkResidue = 0; ;
+            SugarResidue = 0;
+
+            Console.WriteLine("Баланс автомата:{cashBalance}");
+            Console.WriteLine("Колличество воды в автомате:{waterResidue}");
+            Console.WriteLine("Колличество кофе в автомате:{leftoverCoffee}");
+            Console.WriteLine("Колличество молока в автомате:{milkResidue}");
+            Console.WriteLine("Колличество сахара в автомате:{sugarResidue}");
+        }
+        public void ToppingUpMachine(int amount, int literOfWater, int grains, int literOfMilk,int kilogramOfSugar)
+        {
+            CashBalance += amount;
+            WaterResidue += literOfWater;
+            LeftoverCoffee += grains;
+            MilkResidue += literOfMilk;
+            SugarResidue += kilogramOfSugar;
+        }
+       
         public void AddBalance(int amount)
         {
             Console.WriteLine($"Вы внесли {amount}");
             Earnings += amount;
 
         }
-        public double CalculateChange(double productPrice, double cashAmount)
+        public double сalculateChange(double productPrice, double cashAmount)
         {
-            Console.WriteLine("Вставьте деньги в купюроприемник");
-            Dictionary<int, int> numberOfCoins = new Dictionary<int, int>();
             if (cashAmount < productPrice)
             {
                 throw new ArgumentException("Недостаточно наличных для оплаты продукта.");
             }
-            else if(cashAmount> productPrice)
+
+            Dictionary<int, int> numberOfCoins = new Dictionary<int, int>();
+            double change = cashAmount - productPrice;
+            double result = change;
+
+            if (change > 0)
             {
-                double change = cashAmount - productPrice;
-                int OneRuble = 0;
-                int TwoRubles = 0;
-                int FiveRubles = 0;
+                int oneRuble = 0;
+                int twoRubles = 0;
+                int fiveRubles = 0;
 
-
-               while (change >= 1)
+                while (change >= 5)
                 {
-                    change = change - 1;
-                    OneRuble++;
-
+                    change -= 5;
+                    fiveRubles++;
                 }
 
+                while (change >= 2)
+                {
+                    change -= 2;
+                    twoRubles++;
+                }
+
+                while (change >= 1)
+                {
+                    change -= 1;
+                    oneRuble++;
+                }
+                numberOfCoins.Add(1, oneRuble);
+                numberOfCoins.Add(2, twoRubles);
+                numberOfCoins.Add(5, fiveRubles);
+
+                this.NumberOfCoins[1] = this.NumberOfCoins[1] - numberOfCoins[1];
+                this.NumberOfCoins[2] = this.NumberOfCoins[2] - numberOfCoins[2];
+                this.NumberOfCoins[3] = this.NumberOfCoins[3] - numberOfCoins[3];
             }
 
-           
-            return change;
+            return result;
         }
-        public void DisplayPrice(Drink drink)
-        {
-            Console.WriteLine($"Цена напитка" + " " + drink.Name + " " +" "+ drink.Price );
-        }
-        
-        
         public void BuyDrink(Drink drink)
         {
-            Console.WriteLine("Вы выбрали напиток: " + drink.Name);
+            Console.WriteLine("Вы выбрали напиток: " + drink.Name +" "+drink.Price);
             Console.WriteLine("Введите количество сахара для напитка: ");
             int sugarAmount = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Выберите способ оплаты (1 - наличными, 2 - картой): ");
@@ -68,10 +103,22 @@ namespace SaleOfCoffee
 
             if (paymentMethod == 1)
             {
-                CalculateChange()
+                Console.WriteLine("Внесите сумму в купюроприемник");
+                double cashAmount = Convert.ToDouble(Console.ReadLine());
+                try
+                {
+                    double change = сalculateChange(drink.Price, cashAmount);
+                    Console.WriteLine("Ваша сдача: " + change);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(" Oшибка оплаты");
+                }
+
             }
             else if (paymentMethod == 2)
             {
+                Console.WriteLine("Оплата прошла");
             }
             else
             {
@@ -80,6 +127,10 @@ namespace SaleOfCoffee
             }
             Console.WriteLine("Спасибо за покупку!");
         }
+       
+
+        
+
 
     }
 }
